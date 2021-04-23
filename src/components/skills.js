83fe81d2061a239fc2998679c313b48c2a63c875
Juan <jpalenzuela@outlook.com>
2021-04-23 10:58:05 +0100

    finish button resume

--- src/components/skills.js
+++ src/components/skills.js
@@ -3,7 +3,11 @@ import './skills.css';
 const Skills = () => {
     return(
         <>
+
+        <a href= './JuanPalenzuela_CV.pdf'>
         <button>Résumé</button>
+        </a>
+
         <ul>
             <li>Hello</li>
         </ul>
