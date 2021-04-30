4d9182eb04cb9e308eb1a7f03f9208eb2950e253
Juan <jpalenzuela@outlook.com>
2021-04-30 17:45:05 +0100

    change bio

--- src/components/about.js
+++ src/components/about.js
@@ -7,10 +7,10 @@ const About = () => {
     
         <div class="first-column">
         <p>
-        After a one-year stint as an associate at a consultancy, I realised that I wanted to be more directly involved in solving problems and creating products. I wanted a career that was technically challenging.
+        After a one-year stint as an associate at a consultancy, I realised that I wanted to be more directly involved in solving problems and creating products. I wanted a technically challenging career.
         </p>
         <p>
-        I began by learning Python in my spare time and I quickly became enamored with software development. I am fascinated with the fact that you can create powerful tools with very little – you just need a computer. I also love the permissionless leverage that coding gives you.
+        I began by learning Python in my spare time, and I quickly became enamoured with software development. I am fascinated with the fact that you can create powerful tools with very little – you just need a computer. I also love the permissionless leverage that coding gives you.
         </p>
         </div>
 
@@ -19,7 +19,7 @@ const About = () => {
         Having recently completed CodeClan’s 16-week intensive Professional Software Development programme, I have built a strong foundation in Java, Python and Javascript. I have also completed several projects and challenges. You will find these below.
         </p>
         <p>
-        I am continuously looking to learn something new and challenging. I can pick new technologies quickly and on my own, and I am deeply motivated and eager to continue growing as a developer.
+        I am continuously looking to learn something new and challenging. I can pick new technologies quickly, and I am deeply motivated and eager to continue growing as a developer.
         </p>
         </div>
         </div>
